8b1b29dd4385fbff7a0ee8c15b58479d922ca137
charon1212 <charons2turquoise@gmail.com>
2023-12-03 14:33:13 +0900

    実装変更

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -1,16 +1,13 @@
+import { commandInit } from "./init/init";
+
 export type Command = 'init' | 'add';
 export type CommandDispatcher = (args: string[], options: { key: string, value?: string }[]) => unknown;
-const CommandList: Command[] = ['init', 'add'];
+const commandList: { command: Command, dispatcher: CommandDispatcher }[] = [
+  { command: 'init', dispatcher: commandInit },
+];
 
 export const dispatchCommand = (command: Command, args: string[], options: { key: string, value?: string }[],) => {
-  switch (command) {
-    case 'init':
-      break;
-    case 'add':
-      break;
-    default:
-      command satisfies never; // switch-case書き漏れのためのおまじない
-  }
+  commandList.find((v) => v.command === command)?.dispatcher(args, options);
 };
 
-export const isCommand = (args: string): args is Command => CommandList.some((c) => c === args);
+export const isCommand = (args: string): args is Command => commandList.some(({ command }) => command === args);
