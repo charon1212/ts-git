97d89055be3a37c51e8153aef6fae0b5e327c7b6
charon1212 <charons2turquoise@gmail.com>
2023-12-23 21:48:09 +0900

    gitPathをcwd以外からも利用できるよう修正

--- src/domain/gitPath.ts
+++ src/domain/gitPath.ts
@@ -4,21 +4,24 @@ import { GitHash } from './gitObject/gitObject';
 const cwd = process.cwd();
 const DOT_GIT = '.git';
 
-/** 各種パス */
-export const gitPath = {
-  git: {
-    path: join(cwd, DOT_GIT),
-    objects: {
-      path: join(cwd, DOT_GIT, 'objects'),
-    },
-    HEAD: {
-      path: join(cwd, 'HEAD'),
-    },
-    index: {
-      path: join(cwd, 'index'),
-    },
-  },
-};
-
 /** Gitオブジェクトのパス */
 export const getGitPathFromHash = (gitHash: GitHash) => join(gitPath.git.objects.path, gitHash.slice(0, 2), gitHash.slice(2));
+
+/** 各種パス */
+export const getGitPath = (rootDir?: string) => ({
+  git: {
+    path: join(rootDir ?? cwd, '.git'),
+    objects: {
+      path: join(rootDir ?? cwd, '.git', 'objects'),
+    },
+    HEAD: {
+      path: join(rootDir ?? cwd, 'HEAD'),
+    },
+    index: {
+      path: join(rootDir ?? cwd, 'index'),
+    },
+  },
+});
+
+export type GitPath = ReturnType<typeof getGitPath>;
+export const gitPath = getGitPath();
