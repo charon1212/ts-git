b07614d88c5552e296d8c0afa251e831e0c8a15e
charon1212 <charons2turquoise@gmail.com>
2023-12-23 21:48:27 +0900

    sha1を一部修正

--- src/util/sha1.ts
+++ src/util/sha1.ts
@@ -1,6 +1,6 @@
-import { createHash } from "crypto";
+import { BinaryLike, createHash } from "crypto";
 
-export const createSha1 = (data: string) => {
+export const createSha1 = (data: BinaryLike) => {
   const sha1 = createHash("sha1").update(data).digest("hex");
   if (sha1.length !== sha1Length) throw new Error(`sha1の長さが${sha1Length}でない`);
   return sha1;
